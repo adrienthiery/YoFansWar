35e2d8c30cc8ce553abd6e464596653dd1734778
Adrien Chrome <adrien.thiery@gmail.com>
2014-11-14 19:40:04 -0500

    Added mobile css modif

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '', 'auto');
+  ga('create', 'UA-56757965-2', 'auto');
   ga('require', 'displayfeatures');
   ga('send', 'pageview');
 
